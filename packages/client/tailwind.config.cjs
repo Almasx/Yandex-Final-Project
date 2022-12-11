78f089dcdb3537ee2a889decbbef1928dfa5aa59
Tiunov Vladimir <tiunov2008.va@gmail.com>
2022-12-11 18:12:34 +0300

    create section 3 for teams page

--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'black': '#000',
+      'white': '#fff',
+      'white-text': '#FAFAFA',
+      'dark-bg': '#0B0B0B'
+    }
   },
   plugins: [],
 };
