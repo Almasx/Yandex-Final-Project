d26dfc4ea1c397d7fbe0437c29985742fb7faa1c
Tiunov Vladimir <tiunov2008.va@gmail.com>
2022-12-18 21:04:36 +0300

    fix pagination

--- src/components/molecules/ProjectsSlider/index.tsx
+++ src/components/molecules/ProjectsSlider/index.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const ProjectsSlider = () => {
   return (
@@ -16,6 +17,10 @@ const ProjectsSlider = () => {
           nextEl: ".arrow-left",
           disabledClass: "disable",
         }}
+        pagination={{
+          type: "fraction",
+          el: '.swiper-pagination'
+        }}
       >
         {Array(7).fill(
           <SwiperSlide>
@@ -26,7 +31,7 @@ const ProjectsSlider = () => {
       <div className="flex justify-center">
         <div className="border-dark-gray mt-11 flex flex-row items-center gap-[6px] rounded-2xl border p-2">
           <img src="./icons/ArrowRight.svg" alt="" className="arrow-right" />
-          <span className="tracking-widest">1 / 8</span>
+          <div className="swiper-pagination opacity-60 min-w-[40px]"></div>
           <img src="./icons/ArrowLeft.svg" alt="" className="arrow-left" />
         </div>
       </div>
