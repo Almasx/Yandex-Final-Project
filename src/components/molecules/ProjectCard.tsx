63e41260acd35d4c1b141189f902ae45514a294c
Almasx <saparovalmas0@gmail.com>
2022-12-22 10:56:41 +0500

    fix

---
+++ src/components/molecules/ProjectCard.tsx
@@ -0,0 +1,50 @@
+import type { Tech } from "@prisma/client";
+import Badge from "../atoms/Badge";
+
+interface IProjectCardProps {
+  title: string;
+  description: string;
+  link_github: string;
+  author: string;
+  tech_stack: Tech[];
+}
+
+const ProjectCard = ({
+  title,
+  description,
+  link_github,
+  author,
+  tech_stack,
+}: IProjectCardProps) => {
+  return (
+    <div className="border-gray relative w-full rounded-md border p-4">
+      <div className=" h-60 w-full rounded-md bg-black/60 dark:bg-white/30" />
+      <div className="absolute right-16 top-6 flex rounded-xl bg-light-bg py-1 px-3 opacity-80 dark:bg-dark-bg">
+        <img src="./icons/User.png" alt="" />
+        <span className="ml-2 text-black dark:text-white">{author}</span>
+      </div>
+      <a
+        href={link_github}
+        className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-2xl bg-light-bg p-1 opacity-80 dark:bg-dark-bg"
+      >
+        <img src="/icons/github.svg" alt="" />
+      </a>
+      <div className="mt-4 flex flex-col gap-3 py-1">
+        <div className="flex items-center justify-between">
+          <span className="text-3xl">{title}</span>
+          <span className="text-black/30 dark:text-white/30">
+            21/11/2022 | Hackathon
+          </span>
+        </div>
+        <div className="flex flex-row gap-2">
+          {tech_stack.map((tech) => (
+            <Badge key={tech.id} text={tech.title} />
+          ))}
+        </div>
+        <p className="text-black/60 dark:text-white/60">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
