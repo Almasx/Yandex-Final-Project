9a33a44eb97d0d86e7138bdf07a95ccaab962f2e
Almasx <saparovalmas0@gmail.com>
2022-12-22 10:52:43 +0500

    Projects page infinte scroll

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Button from "../components/atoms/Button";
-import ProjectSlider from "../components/molecules/ProjectsSlider";
+import ProjectCard from "../components/molecules/ProjectCard";
 import { trpc } from "../utils/trpc";
 
 const projects = () => {
+  const { data, fetchNextPage, hasNextPage } =
+    trpc.projects.getProjects.useInfiniteQuery(
+      { limit: 2 },
+      { getNextPageParam: (lastProject) => lastProject.nextId }
+    );
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const projects = useMemo(
+    () =>
+      data?.pages.reduce((prev, page) => {
+        return {
+          nextId: page.nextId,
+          projects: [...prev.projects, ...page.projects],
+        };
+      }),
+    [data]
+  );
+
   return (
     <>
       <div className="col-span-6 col-start-4 mt-20 flex flex-col items-center">
@@ -18,8 +37,34 @@ const projects = () => {
           I want to work with you 🡥
         </Button>
       </div>
-
-      <ProjectSlider />
+      <div className="col-span-full">
+        <InfiniteScroll
+          dataLength={projects ? projects.projects.length : 0}
+          next={() => {
+            console.log("lol", hasNextPage);
+            fetchNextPage();
+          }}
+          hasMore={!!hasNextPage}
+          loader={<></>}
+          className="my-20 grid grid-cols-2 gap-12"
+        >
+          {projects &&
+            projects.projects.map((project) => {
+              const { title, description, id, link_github, author, tech } =
+                project;
+              return (
+                <ProjectCard
+                  key={id}
+                  title={title}
+                  description={description}
+                  link_github={link_github}
+                  author={author.name}
+                  tech_stack={tech}
+                />
+              );
+            })}
+        </InfiniteScroll>
+      </div>
     </>
   );
 };
