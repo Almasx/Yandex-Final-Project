1723d618bf4cb12424ae400170d8003c5a44eb82
Almasx <saparovalmas0@gmail.com>
2022-12-22 14:25:10 +0500

    Policy

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Navigation = () => {
+  const [theme, setTheme] = useState<boolean>(false); // default light
+  const colorTheme = theme ? "dark" : "light";
+
+  const onClick = () => {
+    setTheme((previos) => !previos);
+  };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(theme ? "light" : "dark");
+    root.classList.add(colorTheme);
+
+    localStorage.setItem("theme", colorTheme);
+    console.log(colorTheme === "dark" ? "light" : "dark", colorTheme);
+  }, [colorTheme, setTheme]);
+
   return (
     <nav
       className="fixed z-20 flex h-16 w-full items-center justify-center border-b border-b-light-gray-200 bg-white/60 text-sm
     uppercase text-black backdrop-blur-2xl dark:border-b-gray-200 dark:bg-black/60 dark:text-white "
     >
       <div className="flex w-[1200px] flex-row items-center justify-between">
-        <img src="./Logo.svg" alt="" />
+        <div className="w-40">
+          <img src="./Logo.svg" alt="" />
+        </div>
         <div className="flex flex-row gap-5 font-light capitalize text-black/30 dark:text-white/30 ">
           <Link
             href="/"
@@ -28,9 +47,14 @@ const Navigation = () => {
             Team
           </Link>
         </div>
-        <Link href="/contact" className="w-32">
-          <button className="font-medium">Contact</button>
-        </Link>
+        <div className="flex w-40 flex-row items-center gap-3 ">
+          <button onClick={onClick} className="flex flex-row truncate">
+            <span className="mr-1">⬤</span> {theme ? "Dark" : "Light"} Theme
+          </button>
+          <Link href="/contact">
+            <button className="font-medium">Contact</button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
