3e9c30eb4b6049f47d79883b6586fe2f1ca63ae5
Tiunov Vladimir <tiunov2008.va@gmail.com>
2022-12-22 21:23:18 +0300

    member card fix

--- src/components/molecules/MemberCard.tsx
+++ src/components/molecules/MemberCard.tsx
@@ -21,7 +21,7 @@ const MemberCard = ({
 }: IMemberCardProps) => {
   return (
     <div className="flex grow flex-col rounded-xl border border-gray-100 p-2">
-      <div className="relative h-60 grow rounded-xl overflow-hidden">
+      <div className="relative h-60 grow rounded-xl overflow-hidden flex items-center">
         <img className="w-full rounded-xl" src={link_pfp} alt={""} />
 
         <div className="absolute right-2 top-2 flex items-center justify-center gap-2 rounded-2xl bg-light-bg/80 p-1 opacity-80 dark:bg-dark-bg">
@@ -34,7 +34,7 @@ const MemberCard = ({
         </div>
       </div>
       <div className="flex flex-col gap-2 p-3 text-base">
-        <div className="flex flex-row justify-between ">
+        <div className="flex flex-row justify-between">
           {name}
           <span className="text-black/60 dark:text-white/60">{location}</span>
         </div>
