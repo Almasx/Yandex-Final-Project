6b1b74f4de089245a0724bf1e28b39a456f05dc3
Tiunov Vladimir <tiunov2008.va@gmail.com>
2022-12-23 13:54:51 +0300

    fix member loader

--- src/components/templates/MemberSection.tsx
+++ src/components/templates/MemberSection.tsx
@@ -3,13 +3,22 @@ import React from "react";
 import Mode from "../atoms/Mode";
 import MemberCard from "../molecules/MemberCard";
 
-const MemberSection = ({ members = [] }: { members?: Author[] }) => {
+const MemberSection = ({members = [], loading} : { members? : Author[], loading : boolean}) => {
+  
   return (
     <div className="relative col-span-full bg-gradient-to-r from-white via-black/30 to-white pt-[1px] dark:from-black dark:via-white/30 dark:to-black">
       <section className="flex  flex-col gap-16 bg-light-bg py-16 dark:bg-dark-bg">
-        <div className="flex flex-row justify-between">
-          {members &&
-            members.map((member) => (
+          {loading && 
+          <div className="flex flex-row justify-between">
+            <div className="animate-pulse rounded-xl h-24 w-[20%] bg-light-gray-200 dark:bg-gray-200"></div>
+            <div className="animate-pulse rounded-xl h-24 w-[20%] bg-light-gray-200 dark:bg-gray-200"></div>
+            <div className="animate-pulse rounded-xl h-24 w-[20%] bg-light-gray-200 dark:bg-gray-200"></div>
+            <div className="animate-pulse rounded-xl h-24 w-[20%] bg-light-gray-200 dark:bg-gray-200"></div>
+          </div>
+          }
+          {!loading && members &&
+          <div className="flex flex-row justify-between">
+            {members.map((member) => (
               <div
                 className="span text-8xl font-bold text-light-gray-200 dark:text-gray-200"
                 key={member.id}
@@ -17,7 +26,7 @@ const MemberSection = ({ members = [] }: { members?: Author[] }) => {
                 {member.name}
               </div>
             ))}
-        </div>
+            </div>}
         <div className="flex w-[500px] flex-col gap-6">
           <h3 className="text-4xl font-bold text-black/60 dark:text-white/60">
             We are team of{" "}
@@ -30,14 +39,8 @@ const MemberSection = ({ members = [] }: { members?: Author[] }) => {
             eu
           </p>
         </div>
-        <div className="grid grid-cols-4 gap-6">
-          {members &&
-            members.map((member) => <MemberCard key={member.id} {...member} />)}
-          <Mode type="memberCard"></Mode>
-          <Mode type="memberCard"></Mode>
-          <Mode type="memberCard"></Mode>
-          <Mode type="memberCard"></Mode>
-        </div>
+        {loading && <div className="grid grid-cols-4 gap-6"><Mode type="memberCard"></Mode><Mode type="memberCard"></Mode><Mode type="memberCard"></Mode><Mode type="memberCard"></Mode></div>}
+        {!loading && <div className="grid grid-cols-4 gap-6">{members.map((member) => <MemberCard key={member.id} {...member} />)}</div>}
       </section>
     </div>
   );
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -5,11 +5,11 @@ import { trpc } from "../utils/trpc";
 import FLogo from "../components/icons/footer-logo";
 const Team = () => {
   const { data } = trpc.author.getAll.useQuery();
-
   return (
     <>
-      <HeroSection />
-      {<MemberSection members={data} />}
+      <HeroSection/>
+      {!data && <MemberSection loading={true} />}
+      {data && <MemberSection members={data} loading={false} />}
       <MapSection />
       <footer className="col-span-full -mx-8">
         <FLogo/>
